59f1781b591a2a543ee121b63fa24431b178010a
nkchambers <nkchames@gmail.com>
2021-11-07 22:10:34 -0800

    initial commit

---
+++ bank_account.py
@@ -0,0 +1,62 @@
+class BankAccount:
+
+
+    def __init__(self, int_rate, account_balance):
+        self.int_rate = int_rate
+        self.account_balance = account_balance
+
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+        return self
+
+
+    def withdraw(self, amount):
+        if BankAccount.can_withdraw(self.account_balance, amount):
+            self.account_balance -= amount
+        else:
+            print ('Insufficient Funds: Charging $5 Fee')
+            self.account_balance -= 5
+
+        return self
+    
+    @staticmethod
+    def can_withdraw(account_balance, amount):
+        if (account_balance - amount) < 0:
+            return False
+        else:
+            return True
+
+
+    def display_account_info(self):
+        print(f"Balance: ${self.account_balance}")
+
+        return self
+
+
+    def yield_interest(self):
+        if BankAccount.can_yield_int(self.account_balance):
+            self.account_balance += (self.account_balance * self.int_rate)
+        else:
+            print('Insufficient Funds')
+        return self
+    
+    @staticmethod 
+    def can_yield_int(account_balance):
+        if account_balance < 0:
+            return False
+        else:
+            return True
+
+
+
+
+# Instantiate Bank Accounts - Create 2 Accounts
+wayne = BankAccount (int_rate = 0.01, account_balance = 0)
+russell = BankAccount(int_rate = 0.01, account_balance = 20000)
+
+
+
+wayne.deposit(5000000).deposit(5000000).deposit(9000000).withdraw(3000000).yield_interest().display_account_info()
+russell.deposit(500000).deposit(700000).withdraw(100000).withdraw(50000).withdraw(50000).withdraw(200000).yield_interest().display_account_info()
